b318406b242a043aa38470262e4b56b9eba5d996
Valentin Rudloff <valentin.rudloff.perso@gmail.com>
2024-12-29 16:20:21 +0100

    max w in news

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -68,7 +68,7 @@ export function Newsletter(props: {
                   type="email"
                   {...field}
                   className={cn(
-                    "sm:rounded-r-none h-10 w-[300px] px-4 py-2",
+                    "sm:rounded-r-none h-10 max-w-[300px] px-4 py-2",
                     props.small && "h-9"
                   )}
                 />
