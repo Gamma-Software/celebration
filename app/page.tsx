087f4649221ba0fe7e007a61a2a2a69455addad3
Valentin Rudloff <valentin.rudloff.perso@gmail.com>
2025-01-04 14:55:32 +0100

    fix redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import { redirect } from "next/navigation";
+"use client";
 import { fromDateToUrlDate } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const today = new Date();
-  const dateString = fromDateToUrlDate(today);
-  console.log(dateString);
-  redirect(`/${dateString}`);
+  const router = useRouter();
+  const params = new URLSearchParams(window.location.search);
+  const userDate = params.get("date")
+    ? new Date(params.get("date")!)
+    : new Date();
+  const dateString = fromDateToUrlDate(userDate);
+  router.push(`/${dateString}`);
 }
