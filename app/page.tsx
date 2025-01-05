b1d29277f37d12af283945de2b8d2461bb16244d
Valentin Rudloff <valentin.rudloff.perso@gmail.com>
2025-01-05 21:18:47 +0100

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 "use client";
+import { useEffect } from "react";
 import { fromDateToUrlDate } from "@/lib/utils";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
-  const params = new URLSearchParams(window.location.search);
-  const userDate = params.get("date")
-    ? new Date(params.get("date")!)
-    : new Date();
-  const dateString = fromDateToUrlDate(userDate);
-  router.push(`/${dateString}`);
+
+  // Use useEffect to access window after component mounts
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const userDate = params.get("date")
+      ? new Date(params.get("date")!)
+      : new Date();
+    const dateString = fromDateToUrlDate(userDate);
+    router.push(`/${dateString}`);
+  }, []);
 }
